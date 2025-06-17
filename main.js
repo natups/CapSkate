6b5f3383dbdc5356b94d1cd2322b3abdd2f65374
MauricioJ57 <jackimauricio578@gmail.com>
2025-06-17 20:02:31 -0300

    calidad a los assets

--- main.js
+++ main.js
@@ -24,6 +24,11 @@ const config = {
       debug: true,
     },
   },
+  render:{
+    pixelArt: true,
+    antialias: false,
+    roundPixels: true
+  },
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
